12262afa6bf948c65ef0ebe10d43d91e1d709d90
smilelikeshit <imammuslim060892@gmail.com>
2021-05-30 23:48:33 +0700

    update

--- main.go
+++ main.go
@@ -21,6 +21,10 @@ func main() {
 
   // Routes
   e.GET("/", hello)
+  e.GET("/v1/video", v1video)
+  e.GET("/v1/phonebook", v1phonebook)
+  e.GET("/v2/video", v2video)
+  e.GET("/v2/phonebook", v2phonebook)
   
 
   // Start server
@@ -32,6 +36,39 @@ func hello(c echo.Context) error {
   
   return c.JSON(http.StatusOK, map[string]string{
     "message" : os.Getenv("HELLO"),
+  })
+  
+}
+
+func v1video(c echo.Context) error {
+  
+  return c.JSON(http.StatusOK, map[string]string{
+    "message" : "this is service v1/video",
+  })
+  
+}
+
+func v1phonebook(c echo.Context) error {
+  
+  return c.JSON(http.StatusOK, map[string]string{
+    "message" : "this is service v1/phonebook",
+  })
+  
+}
+
+
+func v2video(c echo.Context) error {
+  
+  return c.JSON(http.StatusOK, map[string]string{
+    "message" : "this is service v2/video",
+  })
+  
+}
+
+func v2phonebook(c echo.Context) error {
+  
+  return c.JSON(http.StatusOK, map[string]string{
+    "message" : "this is service v2/phonebook",
   })
   
 }
